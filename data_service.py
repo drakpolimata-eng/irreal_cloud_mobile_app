51fb788f00f1d668f66bbc40570d90d4c35c0272
drakpolimata-eng <drakpolimata@gmail.com>
2026-07-08 16:07:46 -0300

    Update data_service.py

--- data_service.py
+++ data_service.py
@@ -69,6 +69,7 @@ def get_professor_classes(professor_id):
         select="id, name, shift, class_code, active, courses(name), professor_id",
         order="name",
         professor_id=professor_id,
+        active=True,
     )
 
 
@@ -124,3 +125,4 @@ def create_challenge_event(challenge_id, actor_id, event_type, notes=""):
             "notes": notes,
         },
     )
+
